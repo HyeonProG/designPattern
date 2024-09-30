0cef9637499083488d112e1e1118edc4b2719d21
HyeonProG <guswo3443@naver.com>
2024-09-30 16:44:43 +0900

    단방향과 양방향 매핑에 대한 이해

---
+++ demo/src/main/java/com/example/demo/model/Order.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "tb_order")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id // PK 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    private String productName;
+    private int price;
+
+
+}
